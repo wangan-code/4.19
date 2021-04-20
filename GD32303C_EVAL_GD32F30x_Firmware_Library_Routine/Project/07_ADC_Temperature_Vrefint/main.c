67bd12b3f5bca8edb761bd2d123b5a726dd148fb
wangan-code <wangan-code@email.com>
2021-04-20 18:25:44 +0800

    commit demo1

--- GD32303C_EVAL_GD32F30x_Firmware_Library_Routine/Project/07_ADC_Temperature_Vrefint/main.c
+++ GD32303C_EVAL_GD32F30x_Firmware_Library_Routine/Project/07_ADC_Temperature_Vrefint/main.c
@@ -5,7 +5,6 @@
 
 /*
     Copyright (C) 2017 GigaDevice
-
     2017-06-23, V1.0.0, demo for GD32F30x
 */
 
@@ -17,8 +16,10 @@
 #define ADC_GPIO_PORT       GPIOC
 #define ADC_GPIO_PIN        GPIO_PIN_3
 
-uint16_t  adc_value;
-float temperature;
+//IN0 :PA0   IN1: PA1  IN2 :PA2 IN3:PA3 
+
+uint16_t  adc_value[5];
+//float temperature;
 float vref_value;
 
 void rcu_config(void);
@@ -34,33 +35,38 @@ void adc_config(void);
 */
 int main(void)
 {
+	
+	int i;
     /* system clocks configuration */
     rcu_config();
     /* configure systick */
     systick_config();  
 	/*GPIO configuration*/
 	  gpio_config();
-	/*DMA configuration*/
-    dma_config();
     /* ADC configuration */
     adc_config();
     /* USART configuration */
     gd_eval_com_init(EVAL_COM1);
-			/* ADC software trigger enable */
-	  adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);     //规则通道
     while(1){
+		dma_config();
 
+		adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);     //规则通道
     /* delay a time in milliseconds */
     delay_1ms(2000);
       
      /* value convert  */
      //  temperature = (1.43 - ADC_IDATA0(ADC0)*3.3/4096) * 1000 / 4.3 + 25;
-    vref_value = adc_regular_data_read(ADC0) * 3.3 / 4096;
+			for ( i=0;i<5;i++){
+			     vref_value = adc_value[i] * 3.3 / 4096;
+				   printf("%5.3fV ", vref_value);
+           
+			}
+			printf("\r\n");
+    
       
      /* value print */
      //  printf(" the temperature data is %2.0f degrees Celsius\r\n", temperature);
-    printf(" the reference voltage data is %5.3fV \r\n", vref_value);
-    printf(" \r\n");
+   
     }
 }
 
@@ -74,6 +80,8 @@ void rcu_config(void)
 {
 	  /* enable GPIOA clock */
     rcu_periph_clock_enable(RCU_GPIOC);
+	  rcu_periph_clock_enable(RCU_GPIOA);
+	  
     /* enable ADC clock */
     rcu_periph_clock_enable(RCU_ADC0);
     /* config ADC clock */
@@ -94,6 +102,10 @@ void gpio_config(void)
 {
     /* config the GPIO as analog mode */
     gpio_init(ADC_GPIO_PORT, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, ADC_GPIO_PIN);
+	  gpio_init(GPIOA,GPIO_MODE_AIN,GPIO_OSPEED_50MHZ,GPIO_PIN_0);
+	  gpio_init(GPIOA,GPIO_MODE_AIN,GPIO_OSPEED_50MHZ,GPIO_PIN_1);
+	  gpio_init(GPIOA,GPIO_MODE_AIN,GPIO_OSPEED_50MHZ,GPIO_PIN_2);
+	  gpio_init(GPIOA,GPIO_MODE_AIN,GPIO_OSPEED_50MHZ,GPIO_PIN_3);
 }
 
 void dma_config(void)
@@ -108,16 +120,16 @@ void dma_config(void)
     dma_data_parameter.periph_addr  = (uint32_t)(&ADC_RDATA(ADC0));
     dma_data_parameter.periph_inc   = DMA_PERIPH_INCREASE_DISABLE;
     dma_data_parameter.memory_addr  = (uint32_t)(&adc_value);
-    dma_data_parameter.memory_inc   = DMA_MEMORY_INCREASE_DISABLE;
+    dma_data_parameter.memory_inc   = DMA_MEMORY_INCREASE_ENABLE;
     dma_data_parameter.periph_width = DMA_PERIPHERAL_WIDTH_16BIT;
     dma_data_parameter.memory_width = DMA_MEMORY_WIDTH_16BIT;  
     dma_data_parameter.direction    = DMA_PERIPHERAL_TO_MEMORY;
-    dma_data_parameter.number       = 1;
+    dma_data_parameter.number       = 5;
     dma_data_parameter.priority     = DMA_PRIORITY_HIGH;
     dma_init(DMA0, DMA_CH0, &dma_data_parameter);
-    dma_circulation_enable(DMA0, DMA_CH0);
-  
-    /* enable DMA channel */
+    dma_circulation_disable(DMA0, DMA_CH0);
+		dma_memory_to_memory_disable(DMA0, DMA_CH0);
+			/* enable DMA channel */
     dma_channel_enable(DMA0, DMA_CH0);
 }
 void adc_config(void)
@@ -127,16 +139,20 @@ void adc_config(void)
     /* ADC mode config */
     adc_mode_config(ADC_MODE_FREE);    //配置ADC同步模式
     /* ADC contineous function enable */
-    adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, ENABLE);   // ADC连续模式使能
+    adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, DISABLE);   // ADC连续模式使能
     /* ADC scan mode disable */
-    adc_special_function_config(ADC0, ADC_SCAN_MODE, DISABLE);  
+    adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE);  
     /* ADC data alignment config */
     adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);   //ADC数据右对齐配置
     /* ADC channel length config */
-    adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 1);    //ADC通道长度配置
+    adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 5);    //ADC通道长度配置    w五路通道
  
-    /* ADC regular channel config */
-    adc_regular_channel_config(ADC0, 0,  ADC_CHANNEL_13 , ADC_SAMPLETIME_55POINT5);//采样时间；55.5个ADC工作周期
+    /* ADC regular channel config */       //规则通道配置
+    adc_regular_channel_config(ADC0, 4,  ADC_CHANNEL_0 , ADC_SAMPLETIME_55POINT5);//采样时间；55.5个ADC工作周期
+	  adc_regular_channel_config(ADC0, 3,  ADC_CHANNEL_1 , ADC_SAMPLETIME_55POINT5);
+	  adc_regular_channel_config(ADC0, 2,  ADC_CHANNEL_2 , ADC_SAMPLETIME_55POINT5);
+	  adc_regular_channel_config(ADC0, 1,  ADC_CHANNEL_3 , ADC_SAMPLETIME_55POINT5);
+	  adc_regular_channel_config(ADC0, 0,  BOARD_ADC_CHANEL, ADC_SAMPLETIME_55POINT5);
     /* ADC trigger config */
     adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL, ADC0_1_2_EXTTRIG_REGULAR_NONE);//触发条件 none是不使用外部触发，只使用软件触发
     adc_external_trigger_config(ADC0, ADC_REGULAR_CHANNEL, ENABLE);   //配置adc外部触发
@@ -146,11 +162,9 @@ void adc_config(void)
     delay_1ms(1);
     /* ADC calibration and reset calibration */
     adc_calibration_enable(ADC0);
-
-    /* ADC DMA function enable */
+		/* ADC DMA function enable */
     adc_dma_mode_enable(ADC0);
-    /* ADC software trigger enable */
-    adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
+
 }
 
 /* retarget the C library printf function to the USART */
@@ -163,5 +177,3 @@ int fputc(int ch, FILE *f)
     while (RESET == usart_flag_get(EVAL_COM1,USART_FLAG_TBE));
     return ch;
 }
-
-
